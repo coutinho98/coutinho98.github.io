0c6bdbfe6995af0ffb8c00f9e3cdbbcf3ce2ef78
coutinho98 <mateuscouto98@outlook.com>
2025-06-21 11:52:35 -0300

    change vite config

--- src/pages/showcase.jsx
+++ src/pages/showcase.jsx
@@ -13,6 +13,15 @@ const Showcase = () => {
                     { children: "github", href: "https://github.com/coutinho98/ShadysDownloader" },
                 ]}
             />
+             <Item
+                name="Shadys Media Donwloader"
+                description="A desktop app to download and watch your favorite videos offline from multiple platforms."
+                imageSrc={shadys}
+                tags={["desktop application", "video downloader"]}
+                links={[
+                    { children: "github", href: "https://github.com/coutinho98/ShadysDownloader" },
+                ]}
+            />
         </main>
     )
 }
