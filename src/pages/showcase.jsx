055fe27ab5a465f6bf9c36fe552d8890556c6e28
coutinho98 <mateuscouto98@outlook.com>
2025-06-21 13:50:15 -0300

    feat: security

--- src/pages/showcase.jsx
+++ src/pages/showcase.jsx
@@ -8,16 +8,7 @@ const Showcase = () => {
                 name="Shadys Media Donwloader"
                 description="A desktop app to download and watch your favorite videos offline from multiple platforms."
                 imageSrc={shadys}
-                tags={["desktop application", "video downloader"]}
-                links={[
-                    { children: "github", href: "https://github.com/coutinho98/ShadysDownloader" },
-                ]}
-            />
-             <Item
-                name="Shadys Media Donwloader"
-                description="A desktop app to download and watch your favorite videos offline from multiple platforms."
-                imageSrc={shadys}
-                tags={["desktop application", "video downloader"]}
+                tags={["python", "yt-dlp api", "desktop application", "video/audio manipulation"]}
                 links={[
                     { children: "github", href: "https://github.com/coutinho98/ShadysDownloader" },
                 ]}
